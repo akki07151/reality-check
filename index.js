f65342cccff2105b4539308048fdaa35842727bb
Akash Prajapati <107127728+akki07151@users.noreply.github.com>
2022-12-19 10:57:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+alert("Hello! this is Akki");
+alert("This is a reality check game");
+
+
+
+var whom = prompt("with whom you want to play i.e friend,family,love, etc.");
+var YourName = prompt("Enter your name");
+var WhomName = prompt("enter your " + whom + " name" );
+var What = prompt("what do you want to reveal i.e love, hate,  etc.");
+var Number = Math.floor(Math.random()*100);
+alert(WhomName + " " + What + "s" + " " + YourName + " " + Number + "%"  ); 
+
+
+
+
+
+
+
